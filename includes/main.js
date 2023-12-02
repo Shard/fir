5a632befa1ce612eeead16caa297742c77d54766
Mark Beukers <mark@afrotoss.com>
2023-12-02 18:26:45 +1000

    Revert "Naval Update"

--- includes/main.js
+++ includes/main.js
@@ -34,4 +34,8 @@ await front.init(res, ICON_MODEL_URL, QUANTITY_MODEL_URL);
 
   front.registerDefaultListeners();
   front.addInputListener(input);
+  front.addDownloadCollageListener(downloadCollage);
+  front.addDownloadTotalsListener(downloadTotals);
+  front.addDownloadTSVListener(downloadTSV);
+  await front.addAppendGoogleListener(appendGoogle);
 })();
